fdd862c2950ba4c8cbcb0bff1885e0b729ac7590
John Reyes <john.jreye@gmail.com>
2019-08-25 04:34:00 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,32 +1,63 @@
 def base_hash {
 	:Yankees => {
-		"New York" => {
-			:sport => "Baseball"
+		"New York Yankees" => {
+			:borough => "Bronx"
 		}
 	}
 }
-
 end
 
-def monopoly_with_second_tier
+def monopoly_with_second_tier = {
 	:Yankees => {
 		"New York" => {
-			:sport => "Baseball"
-		}
+			:borough => "Bronx"
+		},
+			"Accolades" => {
+				:titles "27",
+				:pennants "40"
+			}
 	}
 }
 end
 
-def monopoly_with_third_tier
-  # When you start writing the implementation for this method, copy the Hash
-  # you built in the previous method. Run the tests. They will fail, but
-  # they'll guide you in how to modify what you just did to have more
-  # complexity.
+def monopoly_with_third_tier = {
+	:Yankees => {
+		"New York" => {
+			:borough => "Bronx"
+		},
+			"Accolades" => {
+				:titles "27",
+				:pennants "40"
+			},
+			"Retired Greats" => {
+				:number "21",
+				:lowest_number "1",
+				:highest_number "51"
+			}
+	}
+}
 end
 
-def monopoly_with_fourth_tier
-  # When you start writing the implementation for this method, copy the Hash
-  # you built in the previous method. Run the tests. They will fail, but
-  # they'll guide you in how to modify what you just did to have more
-  # complexity.
+def monopoly_with_fourth_tier = {
+	:Yankees => {
+		"New_York" => {
+			:borough => "Bronx"
+		},
+			"Accolades" => {
+				:titles "27",
+				:pennants "40"
+			},
+			"Retired_Greats" => {
+				:number "21",
+				:lowest_number "1",
+				:highest_number "51"
+			},
+			"My_Favorite" => {
+				:name "Derek Jeter",
+				:position "SS",
+				:number "2",
+				:year_retired "2017"
+			}
+	}
+}
 end
